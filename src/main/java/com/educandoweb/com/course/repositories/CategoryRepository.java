2b01c5552f191a5d518836d4dd58f20ad9437813
Luan Dias <luandsvt@gmail.com>
2024-10-11 17:19:57 -0300

    Category

---
+++ src/main/java/com/educandoweb/com/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.com.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.com.course.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
